44d92b1cd174730fef05bfb7a3eed5e5ab05ce03
Mark <mark.oconnor14@gmail.com>
2025-01-13 18:11:48 +0800

    make a contact form with some simple errors

--- main.go
+++ main.go
@@ -1,28 +1,82 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "html/template"
+	"fmt"
+	"html/template"
+	"net/http"
 )
 
-func main() {
-  // Routes
-  http.HandleFunc("/", homeHandler)
+var templates = template.Must(template.ParseGlob("./templates/*.html"))
 
-  // Start the server
-  fmt.Println("Server is running on port 8080")
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
+func main() {
+	// Routes
+	http.HandleFunc("/", homeHandler)
+
+	// http.HandleFunc()
+
+	// Start the server
+	fmt.Println("Server is running on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(http.StatusOK)
-  template := template.Must(template.ParseFiles("./templates/index.html"))
-  if err := template.Execute(w, nil);
-  err != nil {
-    http.Error(w, "Error rendering template", http.StatusInternalServerError)
-  }
-}
+	type formValues struct {
+		Name  string
+		Email string
+	}
 
+	type formError struct {
+		Field   string
+		Message string
+	}
+
+	if r.Method == http.MethodGet {
+		data := map[string]interface{}{
+			"Form":   formValues{},
+			"Errors": nil,
+		}
+		if err := templates.ExecuteTemplate(w, "index.html", data); err != nil {
+			http.Error(w, "Error rendering template", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			return
+		}
+		form := formValues{
+			Name:  r.FormValue("name"),
+			Email: r.FormValue("email"),
+		}
+
+		var errors []formError
+
+		if form.Name == "" {
+			errors = append(errors, formError{Field: "name", Message: "Name is required"})
+		}
+
+		if form.Email == "" {
+			errors = append(errors, formError{Field: "email", Message: "Email is required"})
+		}
+
+		if len(errors) > 0 {
+			data := map[string]interface{}{
+				"Form":   form,
+				"Errors": errors,
+			}
+			w.WriteHeader(http.StatusOK)
+			if err := templates.ExecuteTemplate(w, "index.html", data); err != nil {
+				http.Error(w, "Error rendering template", http.StatusInternalServerError)
+			}
+			return
+		}
+
+    // if the form succeeds
+    http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+
+}
