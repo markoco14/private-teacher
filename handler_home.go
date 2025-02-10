d1bd348f7dbbd489baf3521aeb74d3367758feda
Mark <mark.oconnor14@gmail.com>
2025-02-10 17:49:22 +0800

    update about section to use dynamic rendering and data from file

--- handler_home.go
+++ handler_home.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"strings"
 )
+
+type FrequentlyAskedQuestion struct {
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
+
+type PageData struct {
+	Lang string						`json:"lang"`
+	Faq  []FrequentlyAskedQuestion	`json:"faq"`
+	Form formValues					`json:"form"`
+	Errors []formError				`json:"errors"`
+}
+
+func loadFrequentlyAsked() ([]FrequentlyAskedQuestion, error) {
+	data, err := os.ReadFile("./data/faq.json")
+	if err != nil {
+		fmt.Println("Error reading file")
+		return nil, err
+	}
+
+	var faqs []FrequentlyAskedQuestion
+	err = json.Unmarshal(data, &faqs)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON")
+		return nil, err
+	}
+
+	return faqs, nil
+}
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
@@ -16,14 +48,25 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		lang = "zh"
 	}
 
-	w.WriteHeader(http.StatusOK)
+	faqContent, err := loadFrequentlyAsked()
+	if err != nil {
+		fmt.Println("Error loading FAQs", err)
+		faqContent = []FrequentlyAskedQuestion{}
+	}
 
-	data := map[string]string{
-		"Lang": lang,
+	pageData := PageData{
+		Lang: lang,
+		Faq: faqContent,
+		Form: formValues{},
+		Errors: []formError{},
 	}
 	
-	err := templates.ExecuteTemplate(w, "index.gohtml", data)
+
+	
+
+	err = templates.ExecuteTemplate(w, "index.gohtml", pageData)
 	if err != nil {
+		fmt.Println("Error rendering index template", err)
 		http.Error(w, "Error rendering index template", http.StatusInternalServerError)
 	}
 }
