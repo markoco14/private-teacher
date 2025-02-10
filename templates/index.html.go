0dc4218aabd75dbc7ee9aee7df38b3258e92ddcf
Mark <mark.oconnor14@gmail.com>
2025-02-10 10:42:35 +0800

    move business english content to partial and include chinese langauge

--- templates/index.html.go
+++ templates/index.html.go
@@ -75,7 +75,13 @@
   <body>
     <header class="w-full h-[48px] bg-deep text-white flex items-center z-10">
         <div class="flex justify-between w-full max-w-[1000px] mx-auto px-4 xl:px-0">
-            <span>Teacher Mark</span>
+            <span>
+                {{ if eq .Lang "en" }}
+                    Teacher Mark
+                {{ else }}
+                    老師馬克
+                {{ end }}
+            </span>
             <nav>
                 <a href="/en" class='{{if eq .Lang "en"}} underline underline-offset-2 {{ end }}'>English</a>
                 <a href="/" class='{{if eq .Lang "zh"}} underline underline-offset-2 {{ end }}'>中文</a>
@@ -85,86 +91,7 @@
     <main>
         {{ template "hero" . }}
         {{ template "test-prep" . }}
-        <section id="business-english" class="min-h-[50vh] bg-purple-200 grid place-content-center py-32">
-            <div class="hide">
-                <div class="px-4 max-w-[600px] mx-auto">
-                    <h2 class="font-bold text-deep text-5xl text-balance md:text-center mb-8">Professional English classes for career success.</h2>
-                    <p class="text-lg text-gray-800 mb-4">Our classes are customized for your personal needs and are designed to build well-rounded English skills and instill confidence with professional communication in English.</p>
-                </div>
-
-                <ul class="px-2 max-w-[800px] mx-auto grid grid-rows-4 sm:grid-rows-3 md:grid-rows-2 sm:grid-cols-6 md:grid-cols-8 gap-4 mb-16">
-                    <li class="row-span-1 md:row-span-2 stacked rounded-xl sm:col-span-6 md:col-span-8 h-[200px]">
-                        <div class="w-full h-full grid sm:grid-cols-2 md:grid-cols-3 sm:bg-purple-950 rounded-xl">
-                            <div class="stacked sm:col-start-2 sm:col-span-2">
-                                <img
-                                    height="200"
-                                    widht="200"
-                                    loading="lazy"
-                                    fetchpriority="low"
-                                    src="https://teacher-mark.s3.ap-southeast-1.amazonaws.com/small-bus-eng-presentation.jpg" 
-                                    class="w-full object-cover h-[200px] rounded-xl sm:rounded-tr-xl sm:rounded-br-xl"/>
-                                <div class="bg-purple-950/50 sm:bg-gradient-to-r sm:from-purple-950 sm:to-transparent rounded-xl sm:rounded-tr-xl sm:rounded-br-xl"></div>
-                            </div>
-                        </div>
-                        <div class="flex flex-col justify-end p-4">
-                            <h3 class="text-2xl font-semibold text-white">Communicate with <br> Clients and Colleagues</h3>
-                        </div>
-                    </li>
-                    <li class="row-span-1 md:row-span-2 stacked bg-white rounded-xl sm:col-span-3 md:col-span-2 h-[200px]">
-                        <div class="w-full h-full">
-                            <img
-                                height="200"
-                                wids="200"
-                                loading="lazy"
-                                fetchpriority="low" 
-                                src="https://teacher-mark.s3.ap-southeast-1.amazonaws.com/small-bus-eng-write-effectively.jpg" 
-                                class="w-full object-cover h-[200px] rounded-xl"/>
-                        </div>
-                        <div class="w-full h-full bg-purple-900/50 rounded-xl"></div>
-                        <div class="flex flex-col justify-end p-4">
-                            <h3 class="text-xl text-white font-semibold">Write <br> Effectively</h3>
-                        </div>
-                    </li>
-                    <li class="row-span-1 md:row-span-2 stacked bg-white rounded-xl sm:col-span-3 md:col-span-2 h-[200px]">
-                        <div class="w-full h-full">
-                            <img
-                                height="200"
-                                width="200"
-                                loading="lazy"
-                                fetchpriority="low"
-                                src="https://teacher-mark.s3.ap-southeast-1.amazonaws.com/small-bus-eng-understand.jpg"
-                                class="w-full object-cover h-[200px] rounded-xl"/>
-                        </div>
-                        <div class="w-full h-full bg-purple-900/50 rounded-xl"></div>
-                        <div class="flex flex-col justify-end p-4">
-                            <h3 class="text-xl text-white font-semibold">Understand <br> Native Speakers</h3>
-                        </div>
-                    </li>
-                    <li class="row-span-1 md:row-span-2 stacked bg-white rounded-xl sm:col-span-6 md:col-span-4 h-[200px]">
-                        <div class="w-full h-full">
-                            <img
-                                height="200"
-                                width="200"
-                                loading="lazy" 
-                                fetchpriority="low"
-                                src="https://teacher-mark.s3.ap-southeast-1.amazonaws.com/small-bus-eng-master-industry.jpg"
-                                class="w-full object-cover h-[200px] rounded-xl"/>
-                        </div>
-                        <div class="w-full h-full bg-purple-900/50 rounded-xl"></div>
-                        <div class="flex flex-col justify-end p-4">
-                            <h3 class="text-xl font-semibold text-white">Master <br> Industry English</h3>
-                        </div>
-                    </li>
-                </ul>
-
-                <div class="flex justify-center gap-4">
-                    <a href="#contact" class="inline-block bg-purple-800 px-8 py-2 text-white rounded-md hover:bg-purple-700 active:bg-purple-900 active:scale-95 duration-200 ease-in-out">
-                        Book Now
-                    </a>
-                    <a href="#business-english" class="inline-block text-lg bg-purple-300 text-purple-800 rounded-md px-4 py-1 hover:bg-purple-200 active:bg-purple-400 active:text-purple-900 active:scale-95 duration-200 ease-in-out">Learn More</a>
-                </div>
-            </div>
-        </section>
+        {{ template "business-english" . }}        
 
         <section id="about">
             <div class="hide max-w-[1000px] mx-auto grid sm:grid-cols-2">
