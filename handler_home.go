4b998c8117248ba6104d4444a20603706f263067
Mark <mark.oconnor14@gmail.com>
2025-09-01 10:56:22 +0800

    move page and faq content functions to utils file

--- handler_home.go
+++ handler_home.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 )
 
@@ -22,95 +20,6 @@ type PageData struct {
 	Errors []formError				`json:"errors"`
 }
 
-func getFaqContent(location string) (string, error) {
-	content, err := os.ReadFile(location)
-	if err != nil {
-		return "", fmt.Errorf("error reading file %s: %w", location, err)
-	}
-	return string(content), nil
-}
-
-func createFaqList(content string) ([]FrequentlyAskedQuestion, error) {
-	var faqs []FrequentlyAskedQuestion
-	lines := strings.Split(content, "\n")
-
-	var currentFAQ *FrequentlyAskedQuestion
-	var isQuestion bool
-
-	for _, line := range lines {
-		trimmedLine := strings.TrimSpace(line)
-		if trimmedLine == "" {
-			continue // skip empty lines
-		}
-
-		if strings.HasPrefix(trimmedLine, "---") && strings.HasSuffix(trimmedLine, "---") {
-			header := strings.Trim(trimmedLine, "-")
-			if strings.HasPrefix(header, "Q_") {
-				if currentFAQ != nil {
-					faqs = append(faqs, *currentFAQ)
-				}
-				currentFAQ = &FrequentlyAskedQuestion{}
-				isQuestion = true
-			} else {
-				isQuestion = false
-			}
-		} else {
-			if currentFAQ != nil {
-				if isQuestion {
-					currentFAQ.Question += trimmedLine
-				} else {
-					if currentFAQ.Answer != "" {
-						currentFAQ.Answer += " "
-					}
-					currentFAQ.Answer += trimmedLine
-				}
-			}
-		}
-	}
-
-	if currentFAQ != nil {
-		faqs = append(faqs, *currentFAQ)
-	}
-
-	return faqs, nil
-}
-
-func getPageContent(location string) (map[string]string, error) {
-	file, err := os.Open(location)
-	if err != nil {
-		log.Printf("Error opening file: %v", err)
-		return nil, err 
-	}
-	defer file.Close()
-	
-	contentMap := make(map[string]string)
-	scanner := bufio.NewScanner(file)
-
-	var currentHeader string
-	for scanner.Scan() {
-		line := strings.TrimSpace((scanner.Text()))
-		if line == "" {
-			continue // skip empty lines
-		}
-
-		// check if line has header
-		if strings.HasPrefix(line, "---") && strings.HasSuffix(line, "---") {
-			// extract the header key
-			currentHeader = strings.Trim(line, "-")
-			currentHeader = strings.ToLower(currentHeader)
-		} else {
-			// append the content to the current header's value
-			contentMap[currentHeader] += line
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Printf("Error reading content from file: %v", err)
-		return nil, err
-	}
-
-	return contentMap, nil
-}
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
